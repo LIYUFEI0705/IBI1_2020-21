e3c7837be584ae124826ca91ace3f97b4fbb38af
LIYUFEI0705 <79954961+LIYUFEI0705@users.noreply.github.com>
2021-03-10 17:06:38 +0800

    practical5

---
+++ practical5/r_rate.py
@@ -0,0 +1,10 @@
+# remember r is the people that will be infected, don not forget to add the ori$
+n=84
+r=1.5
+i=1
+for i in range(1,6):
+    n=n*(r+1)
+
+print("Every patient can infects average",r," normal individuals.When there are$
+
+
