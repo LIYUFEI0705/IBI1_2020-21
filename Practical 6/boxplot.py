d9de326694527aa1832618840415ca22dfd0c14b
LIYUFEI0705 <79954961+LIYUFEI0705@users.noreply.github.com>
2021-03-20 00:14:22 +0800

    practical 6

---
+++ Practical 6/boxplot.py
@@ -0,0 +1,17 @@
+import numpy as np
+gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
+exon_counts=[51,1142,42,216,25,650,32533,57,1,523]
+a=np.array(gene_lengths)# use the functions in numpy to create an array
+b=np.array(exon_counts)
+average_exonlengths=a/b
+average_exonlengths.sort()#use sort() to sort data
+import matplotlib.pyplot as plt
+plt.boxplot(average_exonlengths)
+# use data in average_exonlengths to create a boxplot
+plt.ylabel("average exon lengths")#define the y axis in boxplot
+plt.xlabel("different genes")#define the x axis in boxpl
+plt.title('The distribution of the average exon lengths of 10 different genes')
+plt.boxplot(average_exonlengths,notch=False,medianprops={'color':'red'},boxprops=dict(color="blue"), whiskerprops = {'color': "black"},capprops = {'color': "cyan"},flierprops={'color':'purple','markeredgecolor':"purple"})
+#change the color of medianprops and boxprops, whiskerprops,cappropos, learnt from:http://blog.sina.com.cn/s/blog_7a6e79df0102xgw0.html
+plt.show()#show the boxplot
+  
---
+++ Practical 6/pie.py
@@ -0,0 +1,16 @@
+DIC={'USA':29862124,'India':11285561,'Brazil':11205972,'Russia':4360823,'UK':4234924}
+import matplotlib.pyplot as plt
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'USA', 'India', 'Brazil', 'Russia','UK'#give every part a name
+sizes = [29862124, 11285561, 11205972, 4360823,4234924]# the data distributed in pie chart
+explode = (0,0,0,0,0)  # Set each item n radii from the center of the circle
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)#draw a pie chart
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title('The proportion of COVID-19 infections from 5 countries')
+plt.show()#show the pie chart
+#we use the example of pie chart from https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_features.html#sphx-glr-gallery-pie-and-polar-charts-pie-features-py
+#and use the data from our practical 
+
