48f44870163f2d97def6dd2f0fc03066ce5b7ff1
LIYUFEI0705 <79954961+LIYUFEI0705@users.noreply.github.com>
2021-05-04 09:55:48 +0800

    Practical 11

---
+++ practical 11/Alignment.py
@@ -0,0 +1,79 @@
+import re
+import os
+#open the three file
+SOD2_mouse=open('SOD2_mouse.fa','r')
+SOD2_human=open('SOD2_human.fa','r')
+randomseq=open('RandomSeq.fa','r')
+def lines(x):#define a function called lines() to extract the sequence name and the sequence
+    a=''
+    for line in x:
+        if line.startswith('>'):
+            y=re.findall(r'^>(\S+ \S+)',line)#extract sequence name
+            if y==[]:
+                y=re.findall(r'^>(\S+)',line)
+            b=False
+        else:
+            a +=line.strip()#extract sequence in a
+    return (y,a)
+SOD2_mouse1,SOD2_mouse2=lines(SOD2_mouse)#reacll lines() and store the sequence name and sequence
+SOD2_human1,SOD2_human2=lines(SOD2_human)
+randomseq1,randomseq2=lines(randomseq)
+#input amino acid and Blosum62 from https://blog.csdn.net/laixiangwei/article/details/105831376
+amino = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*']
+blosum = [
+[ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+[-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+[-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+[-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+[-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+[-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+[-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+[-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+[-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+[-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+[-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+[-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+[-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+[ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+[ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+[-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+[-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+[ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+[-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+[-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+def score(a,x,b,y): #define score(a,x,b,y) to calculate Blosum62 scores
+    score,percent=0,0
+    for i in range(0,len(x)):
+        pos1=amino.index(x[i])#find the place of amino acid presented in x[i] in amino[]
+        pos2=amino.index(y[i])
+        track1=blosum[int(pos1)][int(pos2)]#find the score in blosum[]
+        score+=track1#calculate the alignment score between two sequence
+        if pos1==pos2:
+            percent+=1
+        percent=percent/len(x)*100
+    print('Sequence1 name:',str(a))#print the seq1 name
+    print('Sequence1:',x)#print seq1
+    print('Sequence2 name:',str(b))
+    print('Sequence2:',y)
+    print('Alignment score:',score)#print score
+    print('The percentage of identical amino acids:',percent,'%')#print the percentage of identical amino acid
+score(SOD2_mouse1,SOD2_mouse2,SOD2_human1,SOD2_human2)
+score(SOD2_mouse1,SOD2_mouse2,randomseq1,randomseq2)
+score(SOD2_human1,SOD2_human2,randomseq1,randomseq2)
+
+
+
+
+
+
+
+
+
+
+
+
